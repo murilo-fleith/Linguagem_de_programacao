da17bffa343a8e4cd1babd17d32b2a06dbd27a6c
murilo-fleith <fleith.murilo@hotmail.com>
2021-04-30 10:52:10 -0300

    add button
    add button pesuisa

--- HomeScreen.js
+++ HomeScreen.js
@@ -7,24 +7,46 @@ class HomeScreen extends React.Component {
     return (
       <View style={styles.container}>
         <TouchableOpacity style={styles.cadastar}>
-        <Button 
-        title = "Cadastrar !"
+        <Button
+        justifyContent= 'center'
+        alignItems = 'center'
+        color= "#00FF00"
+        borderRadius= '2' 
+        title = "Cadastrar"
         onPress={() => this.props.navigation.navigate('Cadastro')}
         />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.alterar}>
-        <Button 
+
+        <TouchableOpacity style={styles.alterar} >
+        <Button
+        justifyContent= 'center'
+        alignItems= 'center'
+        color ="#FA5700"
         title = "Alterar"
         onPress={() => this.props.navigation.navigate('Alterar')}
         />
         </TouchableOpacity>
+
         <TouchableOpacity style={styles.deletar}>
-        <Button 
+        <Button
+        justifyContent= 'center'
+        alignItems= 'center'
+        color= "#FF0000" 
         title = "Deletar"
         onPress={() => this.props.navigation.navigate('Delete')}
         />
+        
         </TouchableOpacity>
 
+        <TouchableOpacity style={styles.pesquisar}>
+        <Button
+        justifyContent= 'center'
+        alignItems= 'center'
+        color= "#2935B9" 
+        title = "Pesquisar"
+        onPress={() => this.props.navigation.navigate('Pesquisa')}
+        />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -51,20 +73,37 @@ const styles = StyleSheet.create({
     marginTop: 90,
     padding: 1,
     color: "#000000",
-    borderRadius: 20
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center' 
   },
+
   alterar:{
-    backgroundColor: "#DDDDDD",
+    backgroundColor: "#FA5700",
     margin: 15,
     padding: 1,
     color: "#000000",
-    borderRadius: 20
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+       
   },
   deletar:{
     backgroundColor: "#FF0000",
     margin: 15,
     padding: 1,
     color : "#000000",
-    borderRadius: 20
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
+  pesquisar:{
+    backgroundColor: "#2935B9",
+    margin: 15,
+    padding: 1,
+    color : "#000000",
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
